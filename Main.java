0f9a66225bb5363c55a996859d145724bc9a31f5
Vaibhav <pdt.vaibhav95@gmail.com>
2024-09-05 17:30:07 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    // CTRL + ~ to open terminal
+    // Always use terminal to run programs. For java the command is "java filename.java"
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
